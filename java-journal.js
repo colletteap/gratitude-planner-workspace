33059b5098586acfe613ac6de36cb45bf427aa70
colletteap <134945299+colletteap@users.noreply.github.com>
2023-06-25 19:58:01 -0230

    Add files via upload

---
+++ java-journal.js
@@ -0,0 +1,18 @@
+function selectJournalPrompt() {
+    var prompts = [
+        "How am I feeling today?",
+        "What do I need today?",
+        "What am I grateful for today?",
+        "What do I want to accomplish today?",
+        "The affirmation that would help me most today is..."
+    ];
+    
+    var randomIndex = Math.floor(Math.random() * prompts.length);
+    return prompts[randomIndex];
+}
+
+// Example usage
+document.addEventListener("click", function() {
+    var clickedPrompt = selectJournalPrompt();
+    console.log(clickedPrompt);
+});
