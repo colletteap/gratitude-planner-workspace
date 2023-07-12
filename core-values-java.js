3e3b4d2115fd64bfb0b5499620b23f880025e4ff
colletteap <134945299+colletteap@users.noreply.github.com>
2023-07-12 13:54:12 -0230

    wokring on save button

--- core-values-java.js
+++ core-values-java.js
@@ -1,1 +1,19 @@
-// more coming
+// Save to Local Storage
+const cvdataInput = document.getElementById("cvinput");
+const cvsaveButton = document.getElementById("cvButton");
+
+cvdataInput.value = localStorage.getItem("savedcvData");
+
+// Function to save input data to local storage
+function savecvData() {
+    // Get the input value
+    const cvdata = cvdataInput.value;
+
+    // Save the data to local storage
+    localStorage.setItem('savedcvData', cvdata);
+
+    // Function to retrieve and display data from local storage
+    function retrievecvData() {
+        // Retrieve the data from local storage
+        const inputData = localStorage.getItem('savedcvData');
+    }};
