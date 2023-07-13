d4b261fe67a883af8f6bb6a0d442fef2ff3a6a1c
colletteap <134945299+colletteap@users.noreply.github.com>
2023-07-13 15:50:18 -0230

    working on fillable list

--- core-values-java.js
+++ core-values-java.js
@@ -1,14 +1,21 @@
-// Save to Local Storage
-const cvdataInput1 = document.getElementById("cvinput1");
-const cvsaveButton = document.getElementById("cvButton");
-
-cvdataInput1.value = localStorage.getItem("cvinput1");
-cvsaveButton.addEventListener("click", savecvData);
-
-// Function to save input data to local storage
-function savecvData() {
-    
-    // Save the data to local storage
-    localStorage.setItem('cvinput1', cvdataInput1.value);
-    
-}
+// List
+document.addEventListener('DOMContentLoaded', function() {
+    const wordBank = document.getElementsByClassName('word');
+    const selectedWords = document.getElementsByClassName('selected-words');
+    let order = 1;
+    let wordCount = 0;
+  
+    wordBank.forEach(function(word) {
+      word.addEventListener('click', function() {
+        if (wordCount < 6) {
+        const selectedWord = document.createElement('div');
+        selectedWord.textContent = word.textContent;
+        selectedWord.classList.add('word');
+        selectedWord.style.order = order;
+        selectedWords.appendChild(selectedWord);
+        order++;
+        wordCount++;
+      }
+      });
+    });
+  }); 
