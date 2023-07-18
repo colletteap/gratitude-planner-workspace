0763387718c41159cad0167005c50f4b82dcaa61
colletteap <134945299+colletteap@users.noreply.github.com>
2023-07-18 12:23:25 -0230

    planning

--- subplan-js.js
+++ subplan-js.js
@@ -1,1 +1,11 @@
 // more to come
+
+// add date to top
+
+// input for each white box
+
+// local storage for all inputs
+
+// when title of day clicked on, it changes color
+
+// seating chart will have a click listener to duplicate, a click listener to move, a click listener for double click to type inside
