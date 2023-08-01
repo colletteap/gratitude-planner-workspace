718011bba94c133174d2ea19449ab823b51841df
colletteap <134945299+colletteap@users.noreply.github.com>
2023-08-01 17:21:00 -0230

    formatting and javascript

--- subplan-js.js
+++ subplan-js.js
@@ -51,6 +51,11 @@ document.getElementById("buttonDay").addEventListener("click", function() {
       // Add it to the classScheduleDays container
       classSchedule.appendChild(newperiod);
     }
+
+    if (newClassSchedule.children.length < MAX_DUPLICATES) {
+        const newperiod = period.cloneNode(true);
+        newClassSchedule.appendChild(newperiod);
+    }
   });
 
 // Working on getting period button to work on duplicated divs
@@ -59,46 +64,47 @@ document.getElementById("buttonDay").addEventListener("click", function() {
     const MAX_DUPLICATES = 12; // Maximum number of duplicates
   
     const period = document.getElementById("period");
-    const newClassSchedule = document.getElementById("newClassSchedule");
+    const classSchedule = document.getElementById("classSchedule");
   
-    if (newClassSchedule.children.length < MAX_DUPLICATES) {
+    if (classSchedule.children.length < MAX_DUPLICATES) {
       // Clone the original classSchedule div
       const newperiod = period.cloneNode(true);
       // Add it to the classScheduleDays container
-      newClassSchedule.appendChild(newperiod);
+      classSchedule.appendChild(newperiod);
     }
   });
   
 // working on seating plan -- drag and disappear issue
 
 document.addEventListener('DOMContentLoaded', function() {
-    const seatingPlan = document.getElementById('containerSeating');
+    const seatingChart = document.getElementById('seatingChart');
     const addSeatBtn = document.getElementById('addSeatBtn');
     const maxSeats = 30;
     let seatCounter = 1;
   
-    function startDrag(event) {
-        const seat = event.target;
-        const initialX = event.clientX;
-        const initialY = event.clientY;
-        const { top, left } = seat.getBoundingClientRect();
-        const offsetX = initialX - left;
-        const offsetY = initialY - top;
-      
-        function handleDrag(event) {
-          const newX = event.clientX - offsetX;
-          const newY = event.clientY - offsetY;
-          seat.style.transform = `translate(${newX}px, ${newY}px)`;
-        }
-      
-        function stopDrag() {
-          document.removeEventListener('mousemove', handleDrag);
-          document.removeEventListener('mouseup', stopDrag);
-        }
-      
-        document.addEventListener('mousemove', handleDrag);
-        document.addEventListener('mouseup', stopDrag);
-      }
+    const seat = document.getElementById('seat');
+    let offsetX, offsetY;
+    let isDragging = false;
+    
+    seat.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      offsetX = e.clientX - seat.getBoundingClientRect().left;
+      offsetY = e.clientY - seat.getBoundingClientRect().top;
+      seat.style.cursor = 'grabbing';
+    });
+    
+    document.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      const x = e.clientX - offsetX;
+      const y = e.clientY - offsetY;
+      seat.style.left = x + 'px';
+      seat.style.top = y + 'px';
+    });
+    
+    document.addEventListener('mouseup', () => {
+      isDragging = false;
+      seat.style.cursor = 'grab';
+    });
   
     addSeatBtn.addEventListener('click', function() {
       if (seatCounter >= maxSeats) return;
@@ -106,12 +112,43 @@ document.addEventListener('DOMContentLoaded', function() {
       newSeat.className = 'seat';
       newSeat.draggable = true;
       newSeat.innerHTML = '<textarea class="seatName" placeholder="Enter name"></textarea>';
-      containerSeating.appendChild(newSeat);
+      seatingChart.appendChild(newSeat);
       seatCounter++;
     });
-  
-    containerSeating.addEventListener('dblclick', duplicateSeat);
-    containerSeating.addEventListener('dragstart', handleDragStart);
+
   });
+
+
+  // trying new code to get drag to work
+
+  function dragstart_handler(seatingChart) {
+    
+    seatingChart.dataTransfer.setData("seat", ev.target.id);
+    seatingChart.dataTransfer.effectAllowed = "move";
+  }
+  function dragover_handler(seatingChart) {
+    seatingChart.preventDefault();
+    seatingChart.dataTransfer.dropEffect = "move";
+  }
+  function drop_handler(seatingChart) {
+    seatingChart.preventDefault();
+    
+    const data = seatingChart.dataTransfer.getData("seat");
+    seatingChart.target.appendChild(document.getElementById(data));
+  }
   
+// Add New Period Planner
+
+document.getElementById("addPeriodPlanBtn").addEventListener("click", function() {
+    const MAX_DUPLICATES = 10;
   
+    const period = document.getElementById("periodDiv");
+    const containerPlan = document.getElementById("containerPlan");
+  
+    if (containerPlan.children.length < MAX_DUPLICATES) {
+      
+      const newperiod = period.cloneNode(true);
+      
+      containerPlan.appendChild(newperiod);
+    }
+  });
